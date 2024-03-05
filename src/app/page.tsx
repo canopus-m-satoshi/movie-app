47cf661436aede908d79f15e9fd8e003dd91cb42
Satoshi Matsushima <work.satoshi1993@gmail.com>
2024-03-05 21:54:24 +0900

    もっと見るボタン実装
    映画タイトル検索時に、結果が20件以上ある場合は【もっと見るボタン】を表示
    追加で読み込めるように実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import axios from 'axios'
 import Image from 'next/image'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import dummyImg from '../../public/assets/images/dummy-image.png'
 
@@ -18,7 +18,10 @@ export default function Home() {
   const [inputedText, setInputedText] = useState('')
   const [searchField, setSearchField] = useState('')
   const [isShowInputedText, setIsShowInputedText] = useState(false)
+  const [isShowLoadButton, setIsShowLoadButton] = useState(false)
   const [movies, setMovies] = useState<Movie[]>([])
+  const [currentPage, setCurrentPage] = useState(1)
+  const [totalPages, setTotalPages] = useState<number>(0)
 
   const poster_url = 'https://image.tmdb.org/t/p/w300_and_h450_bestv2/'
 
@@ -30,17 +33,27 @@ export default function Home() {
     searchMovies()
     setSearchField(inputedText)
     setIsShowInputedText(true)
-    setInputedText('')
+    setCurrentPage((prevPage) => prevPage + 1)
   }
 
   const handleOnClear = () => {
+    setInputedText('')
     setIsShowInputedText(false)
+    setIsShowLoadButton(false)
     setMovies([])
+    setCurrentPage(1)
+    setTotalPages(0)
+  }
+
+  const handleOnLoad = () => {
+    setCurrentPage((prevPage) => prevPage + 1)
+
+    searchMovies()
   }
 
   const searchMovies = async () => {
     const apiToken = process.env.NEXT_PUBLIC_TMDB_API_TOKEN
-    const baseUrl = `https://api.themoviedb.org/3/search/movie?query=${inputedText}&include_adult=false&language=ja&page=1`
+    const baseUrl = `https://api.themoviedb.org/3/search/movie?query=${inputedText}&include_adult=false&language=ja&page=${currentPage}`
 
     try {
       const options = {
@@ -53,16 +66,31 @@ export default function Home() {
 
       const response = await axios.get(baseUrl, options)
 
-      return setMovies(response.data.results)
+      setMovies((prevMovies) => [...prevMovies, ...response.data.results])
+      setTotalPages(response.data.total_pages)
+
+      setIsShowInputedText(true)
     } catch {
       throw new Error('Failed to fetch search results')
     }
   }
 
+  useEffect(() => {
+    if (totalPages >= 1) {
+      setIsShowLoadButton(true)
+    } else {
+      setIsShowLoadButton(false)
+    }
+
+    currentPage > totalPages && setIsShowLoadButton(false)
+  }, [totalPages, currentPage])
+
   return (
     <main className="mt-5">
       <div className="container mx-auto py-8">
-        <h2 className="text-center font-bold text-4xl">映画を検索</h2>
+        <h2 className="text-center font-bold text-4xl">
+          映画を検索 名探偵コナン
+        </h2>
         <div className="flex justify-center gap-2 mt-5 w-fit mx-auto">
           <div className="flex-grow">
             <input
@@ -94,28 +122,40 @@ export default function Home() {
           </div>
         )}
 
-        <div className="grid grid-cols-5 gap-y-10 gap-x-4 ">
-          {movies &&
-            movies.map((movie) => (
-              <div key={movie.id}>
-                <Image
-                  src={
-                    movie.poster_path
-                      ? `${poster_url}${movie.poster_path}`
-                      : dummyImg
-                  }
-                  alt={movie.poster_path ? movie.title : 'ダミー画像'}
-                  width={300}
-                  height={440}
-                />
-                <h3 className="font-bold text-2xl mt-3">{movie.title}</h3>
-
-                <p>
-                  公開日:
-                  {movie.release_date ? movie.release_date : ' 不明'}
-                </p>
+        <div>
+          {movies && (
+            <>
+              <div className="grid grid-cols-5 gap-y-10 gap-x-4">
+                {movies.map((movie) => (
+                  <div key={movie.id}>
+                    <Image
+                      src={
+                        movie.poster_path
+                          ? `${poster_url}${movie.poster_path}`
+                          : dummyImg
+                      }
+                      alt={movie.poster_path ? movie.title : 'ダミー画像'}
+                      width={300}
+                      height={440}
+                    />
+                    <h3 className="font-bold text-2xl mt-3">{movie.title}</h3>
+                    <p>
+                      公開日:
+                      {movie.release_date ? movie.release_date : ' 不明'}
+                    </p>
+                  </div>
+                ))}
               </div>
-            ))}
+
+              {isShowLoadButton && (
+                <button
+                  className="btn btn-primary block w-fit mx-auto mt-6"
+                  onClick={handleOnLoad}>
+                  さらに読み込む
+                </button>
+              )}
+            </>
+          )}
         </div>
       </div>
     </main>

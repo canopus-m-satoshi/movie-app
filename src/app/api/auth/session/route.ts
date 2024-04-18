6e09792ffcd22039ef5feffd1372fa19a20b2331
Satoshi Matsushima <work.satoshi1993@gmail.com>
2024-04-18 15:33:56 +0900

    src/app/api/auth/session/route.ts - try/catchの修正
    createSessionCookie 自体を try catchに変更
    
    最初にsessionCookieの有無を調べていたが、削除
    tryの中にcreateSessionCookieで取得した情報をsessionCookieに格納する変数を移動

--- src/app/api/auth/session/route.ts
+++ src/app/api/auth/session/route.ts
@@ -12,31 +12,20 @@ export async function POST(request: NextRequest) {
   if (!idToken)
     return NextResponse.json({ error: 'Missing idToken' }, { status: 400 })
 
-  const sessionCookie = await createSessionCookie(idToken, { expiresIn })
-
-  if (sessionCookie) {
-    try {
-      cookies().set('__session', sessionCookie, {
-        maxAge: expiresIn,
-        httpOnly: true,
-        secure: true,
-      })
-    } catch (error) {
-      console.error('Error setting session cookie:', error)
-      return NextResponse.json(
-        { error: 'Failed to set session cookie.' },
-        { status: 500 },
-      )
-    }
-  } else {
+  try {
+    const sessionCookie = await createSessionCookie(idToken, { expiresIn })
+    cookies().set('__session', sessionCookie, {
+      maxAge: expiresIn,
+      httpOnly: true,
+      secure: true,
+    })
+  } catch (error) {
+    console.error('Error setting session cookie:', error)
     return NextResponse.json(
-      { error: 'Failed to create session cookie.' },
+      { error: 'Failed to set session cookie.' },
       { status: 500 },
     )
   }
 
-  return NextResponse.json(
-    { success: 'Signed in successfully.' },
-    { status: 201 },
-  )
+  return NextResponse.json({ status: 201 })
 }

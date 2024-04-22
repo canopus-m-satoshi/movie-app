099ffa229e24bbe746307c1f6be6458b79e070b8
Satoshi Matsushima <work.satoshi1993@gmail.com>
2024-04-22 23:40:29 +0900

    ユーザーページのCSS変更
    GridレイアウトからFlexレイアウトに変更

--- src/components/StatsItem.tsx
+++ src/components/StatsItem.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 const StatsItem = ({ title, number }: Props) => {
   return (
-    <div className="sm:row-start-2 lg:row-start-auto flex sm:flex-col items-center gap-2 text-center">
+    <div className="flex min-[450px]:flex-col md:flex-col lg:flex-row items-center gap-2 text-center">
       <div>{title}</div>
       <div className="text-4xl font-bold">{number}</div>
     </div>

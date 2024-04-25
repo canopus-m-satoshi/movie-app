fbf9cbea9533f7a9c3451f66960904714d4d7024
Satoshi Matsushima <work.satoshi1993@gmail.com>
2024-04-25 14:12:32 +0900

    HOMEページにログインボタンを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,28 @@
 import Link from 'next/link'
 
-export default function Home() {
+import { getCurrentUser } from '@/lib/firebase/firebase-admin'
+
+export default async function Home() {
+  const isLoggedIn = await getCurrentUser()
+
   return (
     <main className="mt-5">
       <div className="container mx-auto py-8">
         <h2 className="text-center font-bold text-xl md:text-4xl">
-          映画を検索するアプリです
+          映画検索やお気に入りの映画を登録
         </h2>
 
-        <div className="flex justify-center items-center mt-20">
+        <div className="flex justify-center items-center gap-4 mt-20">
           <Link href={'/movie/'} className="btn btn-active btn-primary">
             映画タイトルで検索する
           </Link>
+          {!isLoggedIn && (
+            <Link
+              href={'/auth/signIn'}
+              className="btn btn-active btn-secondary">
+              ログインする
+            </Link>
+          )}
         </div>
       </div>
     </main>

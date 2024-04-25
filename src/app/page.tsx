69e658ed12e7083eeecda78358eeb274ca8497df
Satoshi Matsushima <work.satoshi1993@gmail.com>
2024-04-25 15:30:23 +0900

    HOMEページリード文追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,21 @@ export default async function Home() {
   return (
     <main className="mt-5">
       <div className="container mx-auto py-8">
-        <h2 className="text-center font-bold text-xl md:text-4xl">
-          映画検索やお気に入りの映画を登録
-        </h2>
+        <div className="text-center">
+          <h2 className="font-bold text-2xl md:text-5xl bg-gradient-to-r from-blue-600 to-red-400 bg-clip-text text-transparent">
+            MOVIE APP
+          </h2>
+
+          <p className="mt-8">
+            映画好きのためのアプリです。
+            <br />
+            映画のタイトルで検索することで、映画の詳細情報を確認できます。
+            <br />
+            お気に入りの映画を登録し、ウォッチリストに追加することで、映画鑑賞の計画が立てられます。
+            <br />
+            マイページでは、登録した映画を一覧で確認でき、感想やメモをコメントとして残すことができます。
+          </p>
+        </div>
 
         <div className="flex justify-center items-center gap-4 mt-20">
           <Link href={'/movie/'} className="btn btn-active btn-primary">
